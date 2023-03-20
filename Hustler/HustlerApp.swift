375643ab581414841a799efdf9f8d56126bc86a8
m-mraisi <mahmoudmraisi2@gmail.com>
2023-03-19 22:26:14 -0400

    Initial Commit

---
+++ Hustler/HustlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HustlerApp.swift
+//  Hustler
+//
+//  Created by Mahmoud Mraisi on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct HustlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
